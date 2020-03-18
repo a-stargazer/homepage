5cce324b6db9f118309c87c16c6b773a858dbcb0
Alice <aliceyang013@gmail.com>
2020-03-18 13:56:21 -0700

    added content, docs, templates, and .py

--- build.py
+++ build.py
@@ -1,8 +1,3 @@
-# templates/top.html + content/index.html + templates/bottom.html > docs/index.html
-# templates/top.html + content/about.html + templates/bottom.html > docs/about.html
-# templates/top.html + content/projects.html + templates/bottom.html > docs/projects.html
-# templates/top.html + content/blog.html + templates/bottom.html > docs/blog.html
-
 # template, top and bottom 
 top = open("templates/top.html").read()
 bottom = open("templates/bottom.html").read()
@@ -28,4 +23,4 @@ open('docs/projects.html', 'w+').write(projects_html)
 
 content = open('content/blog.html').read()
 blog_html = top + content + bottom
-open('docs/blog.html', 'w+').write(blog_html)
+open('docs/git ignoreblog.html', 'w+').write(blog_html)
