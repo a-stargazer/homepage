82360ab78d6ad088391dffeea28298c997522b29
Alice <aliceyang013@gmail.com>
2020-03-18 13:09:28 -0700

    added docs/

--- build.py
+++ build.py
@@ -10,22 +10,22 @@ bottom = open("templates/bottom.html").read()
 content = open("content/index.html").read()
 
 index_html = top + content + bottom
-open("index.html", "w+").write(index_html)
+open("docs/index.html", "w+").write(index_html)
 
 # about 
 
 content = open('content/about.html').read()
 about_html = top + content + bottom
-open('about.html', 'w+').write(about_html)
+open('docs/about.html', 'w+').write(about_html)
 
 # projects
 
 content = open('content/projects.html').read()
 projects_html = top + content + bottom
-open('projects.html', 'w+').write(projects_html)
+open('docs/projects.html', 'w+').write(projects_html)
 
 # blog
 
 content = open('content/blog.html').read()
 blog_html = top + content + bottom
-open('blog.html', 'w+').write(blog_html)
+open('docs/blog.html', 'w+').write(blog_html)
