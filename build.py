8782be073dec030dcf29d85ce7a40476d8716477
Alice <aliceyang013@gmail.com>
2020-03-18 13:30:30 -0700

    added code to build.py

---
+++ build.py
@@ -0,0 +1,26 @@
+# template, top and bottom 
+top = open("templates/top.html").read()
+bottom = open("templates/bottom.html").read()
+
+content = open("content/index.html").read()
+
+index_html = top + content + bottom
+open("docs/index.html", "w+").write(index_html)
+
+# about 
+
+content = open('content/about.html').read()
+about_html = top + content + bottom
+open('docs/about.html', 'w+').write(about_html)
+
+# projects
+
+content = open('content/projects.html').read()
+projects_html = top + content + bottom
+open('docs/projects.html', 'w+').write(projects_html)
+
+# blog
+
+content = open('content/blog.html').read()
+blog_html = top + content + bottom
+open('docs/git ignoreblog.html', 'w+').write(blog_html)
