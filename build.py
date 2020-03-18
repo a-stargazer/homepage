e1778ad926394798ab13ed6b0188bf149a515961
Alice <aliceyang013@gmail.com>
2020-03-18 12:48:22 -0700

    added code to build.py

--- build.py
+++ build.py
@@ -1,4 +1,33 @@
-cat templates/top.html content/index.html templates/bottom.html > docs/index.html
-cat templates/top.html content/about.html templates/bottom.html > docs/about.html
-cat templates/top.html content/projects.html templates/bottom.html > docs/projects.html
-cat templates/top.html content/blog.html templates/bottom.html > docs/blog.html
+# templates/top.html + content/index.html + templates/bottom.html > docs/index.html
+# templates/top.html + content/about.html + templates/bottom.html > docs/about.html
+# templates/top.html + content/projects.html + templates/bottom.html > docs/projects.html
+# templates/top.html + content/blog.html + templates/bottom.html > docs/blog.html
+
+# output: docs/index.html?
+
+# template, top and bottom 
+top = open("templates/top.html").read()
+bottom = open("templates/bottom.html").read()
+
+content = open("content/index.html").read()
+
+index_html = top + content + bottom
+open("docs/index.html", "w+").write(index_html)
+
+# about 
+
+content = open('content/about.html').read()
+about_html = top + content + bottom
+open('docs/about.html', 'w+').write(about_html)
+
+# projects
+
+content = open('content/projects.html').read()
+project_html = top + content + bottom
+open('docs/projects.html', 'w+').write(projects_html)
+
+# blog
+
+content = open('content/blog.html').read()
+blog_html = top + content + bottom
+open('docs/blog.html', 'w+').write(blog_html)
