e48592224eed008b4520357b1f02d5062ad231c5
Alice <aliceyang013@gmail.com>
2020-03-19 21:28:18 -0700

    edited .css

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
-# cat templates/top.html content/index.html templates/bottom.html > docs/index.html
-# cat templates/top.html content/about.html templates/bottom.html > docs/about.html
-# cat templates/top.html content/projects.html templates/bottom.html > docs/projects.html
-# cat templates/top.html content/blog.html templates/bottom.html > docs/blog.html
-
-cat templates/top.html content/index.html templates/bottom.html > index.html
-cat templates/top.html content/about.html templates/bottom.html > about.html
-cat templates/top.html content/projects.html templates/bottom.html > projects.html
-cat templates/top.html content/blog.html templates/bottom.html > blog.html
+cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+cat templates/top.html content/about.html templates/bottom.html > docs/about.html
+cat templates/top.html content/projects.html templates/bottom.html > docs/projects.html
+cat templates/top.html content/blog.html templates/bottom.html > docs/blog.html
