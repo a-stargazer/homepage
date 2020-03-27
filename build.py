93d2665fce9a3d05d3e24397c1595007dec4c886
Alice <aliceyang013@gmail.com>
2020-03-26 19:21:07 -0700

    changes to copyright footer

--- build.py
+++ build.py
@@ -4,23 +4,21 @@ bottom = open("templates/bottom.html").read()
 
 content = open("content/index.html").read()
 
+# index 
 index_html = top + content + bottom
 open("docs/index.html", "w+").write(index_html)
 
 # about 
-
 content = open('content/about.html').read()
 about_html = top + content + bottom
 open('docs/about.html', 'w+').write(about_html)
 
 # projects
-
 content = open('content/projects.html').read()
 projects_html = top + content + bottom
 open('docs/projects.html', 'w+').write(projects_html)
 
 # blog
-
 content = open('content/blog.html').read()
 blog_html = top + content + bottom
-open('docs/git ignoreblog.html', 'w+').write(blog_html)
+open('docs/blog.html', 'w+').write(blog_html)
