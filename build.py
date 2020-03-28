b8b89db6acc29febda3a13774a0175c7806f9a43
Alice <aliceyang013@gmail.com>
2020-03-28 15:05:20 -0700

    refactor into multiple functions

--- build.py
+++ build.py
@@ -1,24 +1,78 @@
-# template, top and bottom 
-top = open("templates/top.html").read()
-bottom = open("templates/bottom.html").read()
+pages = [
+	{
+		"filename": "./content/index.html",
+		"output": "./docs/index.html",
+		"title": "Home",
+	},
+	{
+		"filename": "./content/about.html",
+		"output": "./docs/about.html",
+		"title": "About Me",
+	},
+	{
+		"filename": "./content/projects.html",
+		"output": "./docs/projects.html",
+		"title": "My Projects",
+	},
+    {
+		"filename": "./content/blog.html",
+		"output": "./docs/blog.html",
+		"title": "My Blog",
+	},
+]
 
-content = open("content/index.html").read()
+# loop through list
+def main():
+	for page in pages:
+		file_name = page["filename"]
+		page_output = page["output"]
+		page_title = page["title"]
+		page_content = open(file_name).read()
+		combined_page = apply_template(page_content, page_title)
+		final_output = final_page_output(page_output, combined_page)
 
-# index 
-index_html = top + content + bottom
-open("docs/index.html", "w+").write(index_html)
 
-# about 
-content = open('content/about.html').read()
-about_html = top + content + bottom
-open('docs/about.html', 'w+').write(about_html)
 
-# projects
-content = open('content/projects.html').read()
-projects_html = top + content + bottom
-open('docs/projects.html', 'w+').write(projects_html)
+# replace {{content}} with actual page content to base.html
+def apply_template(page_content, page_title):
+	template = open("./templates/base.html").read()
+	finished_template_with_content = template.replace("{{contents}}", page_content)
+	combined_page = finished_template_with_content.replace("{{title}}", page_title)
+	return combined_page
 
-# blog
-content = open('content/blog.html').read()
-blog_html = top + content + bottom
-open('docs/blog.html', 'w+').write(blog_html)
+# output in the correct directory (/docs)
+def final_page_output(page_output, combined_page):
+	open(page_output, "w+").write(combined_page)
+
+# invoking main
+main()
+print("building the site has succeeded")
+
+# --------------------------------------------------------------------------------
+    
+# Phase 3: String replacement templating
+# Read base.html
+# template = open("./templates/base.html").read()
+
+# # index HTML page
+# index_content = open("content/index.html").read()
+# finished_index_page = template.replace("{{content}}", index_content)
+# open("docs/index.html", "w+").write(finished_index_page)
+
+# # about HTML page
+# about_content = open("content/about.html").read()
+# finished_about_page = template.replace("{{content}}", about_content)
+# open("docs/about.html", "w+").write(finished_about_page)
+
+# # projects HTML page
+# projects_content = open("content/projects.html").read()
+# finished_projects_page = template.replace("{{content}}", projects_content)
+# open("docs/projects.html", "w+").write(finished_projects_page)
+
+# # blog HTML page
+# blog_content = open("content/blog.html").read()
+# finished_blog_page = template.replace("{{content}}", blog_content)
+# open("docs/blog.html", "w+").write(finished_blog_page)
+
+		# content = open("./docs/index.html")
+		# resulting_html_for_index = apply_template(content)
