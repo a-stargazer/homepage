463f1c1451b5f4c7e057f889f4d49ca32dbfe589
Alice <aliceyang013@gmail.com>
2020-04-06 15:33:13 -0700

    changes to build.py

--- build.py
+++ build.py
@@ -1,65 +1,49 @@
-# glob
-
 import glob
-all_html_files = glob.glob("content/*.html")
-print(all_html_files)
-
-# output: ['content/index.html', 'content/projects.html', 'content/about.html', 'content/blog.html']
-
-# -------------------------------------------------
-
-# os
-
 import os
+from jinja2 import Template
 
-file_path = "content/blog.html"
-file_name = os.path.basename(file_path)
-print(file_name)
-name_only, extension = os.path.splitext(file_name)
-print(name_only)
-
-# output: 
-# blog.html
-# blog
-
-
-# -------------------------------------------------
-
-# pages list
+# pages list 
 
 pages = []
-pages.append({
-	"filename": "content/index.html",
-	"title": "Index",
-	"output": "docs/index.html",
-})
+all_html_pages = []
+all_html_pages = glob.glob("content/*.html")
+print(all_html_pages)
+
+# main
+def main():
+    index_html = open("./content/index.html").read()
+    template_html = open("./templates/base.html").read()
+    template = Template(template_html)
+    for page in all_html_files:
+    	# generate docs()
+    	content = open(page).read()
+
+# generate html pages from files in "content/"
+    page = "content/*.html"
+    page_name = os.path.basename(page)
+    name_only, extension = os.path.splitext(page_name)
+    full_page = str("docs/" + name_only + ".html")
+    file_of_full_page = str(name_only + ".html")
+
+    pages.append(file_of_full_page)
+
+# file name: file of full page
+final_html = template.render(
+        content = html,
+	    title = title,
+	    pages = pages,
+	    file_name = full_page,
+)
+
+open(str("docs/" + name_only + ".html"), "w+").write(final_html)
+
 print(pages)
 
-# output:
-# [{'filename': 'content/index.html', 'title': 'Index', 'output': 'docs/index.html'}]
+# ---------- Phase 3 ----------
 
-# -------------------------------------------------
-
-# loop through list
-def main():
-	for page in pages:
-		file_name = page["filename"]
-		page_output = page["output"]
-		page_title = page["title"]
-		page_content = open(file_name).read()
-		combined_page = apply_template(page_content, page_title)
-		final_output = final_page_output(page_output, combined_page)
-
-# replace {{content}} and {{title}} placeholders
-def apply_template(page_content, page_title):
-	template = open("./templates/base.html").read()
-	finished_template_with_content = template.replace("{{content}}", page_content)
-	combined_page = finished_template_with_content.replace("{{title}}", page_title)
-	return combined_page
-
-# output in the correct directory (/docs)
-def final_page_output(page_output, combined_page):
-	open(page_output, "w+").write(combined_page)
+# {% for page in pages %}
+#     <a href="{{page.output_filename}}">{{page.title}}</a>
+# {% endfor %}
 
 # invoking main
 main()
